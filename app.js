c62c72aac746fc1f4d00237e51ab9bf5b753f22d
khadija201 <khadija2qubra@gmail.com>
2024-03-03 22:26:33 +0500

    assidment done

---
+++ app.js
@@ -0,0 +1,149 @@
+//  chapter
+//  statement;
+//  q1
+ var city= prompt("Enter your city name")
+if (city === "karachi"){
+     alert("welcome to city of light !")
+ }
+// // q2
+ var gender = prompt("Enter your gender ");
+ if (gender === "male"){
+     alert ("Good Morning sir!")
+}
+ else if(gender === "female"){
+     alert ("Good Morning maam!")
+     }
+ else{
+       alert("Good Morning")
+ }
+// // q3
+ var color = prompt("Enter your  color");
+ if (color === "Red"){
+     alert("please stop")
+ }
+ else if(color === "yellow"){
+     alert("Ready to move")
+ }
+ else if (color === "Green"){
+     alert("Move now")
+ }
+// // q4
+var fuel = +prompt("Enter y fuel")
+ if (fuel ===" < 0.25" ){
+     alert ("please fill the fuel in your car !")
+ } 
+// // q5
+ var a = 4;
+  if (++a === 5){
+     alert("given condition for variable is a true")
+  }
+ var b = 82;
+ if (b++ ===83){
+     alert ("give codition for variable is true")
+ }
+ var c = 12;
+ if (c++ === 13){
+     alert("condition 1 is true")
+ }
+
+ if (c === 13){
+     alert("condition 2 is true")
+ }
+ if (++c < 14){
+     alert("condition 3 is true")
+ }
+
+ if (c === 14){
+     alert("condition 4 is true")
+ }
+ var marterialCost = 20000;
+ var laborCost= 2000;
+  var totalCost= marterialCost + laborCost;
+  if (totalCost === "laborCost + marterialCost"){
+ alert(" the cost equal");
+  }
+
+ if ("car" < "cat"){
+     alert ("car is smaller than cat")
+  }
+// //  q6
+var T = prompt("enter temperature")
+ if (T  > 40){
+     alert("It is too hot outside")
+ }
+ else if (T > 30){
+     alert ("The weather is normal")
+ }
+ else if (T > 20){
+   alert ("Today cool")
+ }
+    else  if(T >10){
+     alert ("OMG! weather is so cool ")
+}
+// chapter 10
+// 1
+var city= "karachi";
+if (city = "karachi"){
+    console.log ("the city of light");
+}
+var eng= +prompt("Enter your english marks");
+var urdu = +prompt("Enter your urdu marks");
+var math = +prompt("Enter your math marks");
+var obtainedMarks= eng + urdu + math ;
+var perc =(obtainedMarks / 300 )* 100;
+
+if (perc <= 100 && perc >= 80){
+    document.write ("grade A")
+}
+else if  (perc <= 79 && perc >= 70){
+    document.write ("grade B")
+}
+else if (perc <= 69 && perc >= 60){
+    document.write ("grade C")
+}
+else if (perc <= 50 && perc >= 40){
+    document.write ("FAIL")
+}
+
+var city =prompt("Enter a city")
+if ( city ==="lahore"){
+    alert("acknowledge it is Lahore ")
+}
+else if ( city ==="karachi"){
+    alert("acknowledge it is Karachi ")
+}
+else  {
+    alert("check the city name ")
+}
+var x = "4" , y = "5", a= "5",b="2", c="7"; 
+if ((x === y || a === b) && c === d){
+    g === h
+}
+else { alert("ok")} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
